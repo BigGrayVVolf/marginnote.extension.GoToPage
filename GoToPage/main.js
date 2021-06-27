00f30da38a4f5a845c3802c9ef0a472b8fc3e61c
BigGrayVVolf <marcosayu@163.com>
2021-06-27 10:50:17 +0800

    init

---
+++ GoToPage/main.js
@@ -0,0 +1,105 @@
+JSB.newAddon = function(mainPath) {
+    var pageNoOffsets = {};
+    var currentDocmd5;
+
+    let tapBlock = function(alert) {
+        JSB.log('MNLOG tapBlock: arg1: %@', alert);
+        let text = alert.textFieldAtIndex(0).text;
+        JSB.log('MNLOG settingAlert text: %@', text);
+        let texts = text.split('@');
+
+        var realPageNo;
+        if (texts.length == 2) {
+            if (!isNaN(parseInt(texts[0]))) {
+                pageNoOffsets[currentDocmd5] = parseInt(texts[0]);
+                JSB.log('MNLOG pageNoOffsets keys: %@', Object.keys(pageNoOffsets).toString());
+                NSUserDefaults.standardUserDefaults().setObjectForKey(pageNoOffsets, 'GoToPage.Offsets');
+            }
+            if (!isNaN(parseInt(texts[1]))) {
+                realPageNo = parseInt(texts[1]);
+            }
+        } else if (texts.length == 1) {
+            if (!isNaN(parseInt(texts[0]))) {
+                realPageNo = parseInt(texts[0]);
+            }
+        }
+
+        if (realPageNo && pageNoOffsets[currentDocmd5]) {
+            let fsbc = Application.sharedInstance().studyController(self.window);
+            let gotoAlert = UIAlertView.makeWithTitleMessageDelegateCancelButtonTitleOtherButtonTitles("Go To Page", "", fsbc, "Real Go", []);
+            gotoAlert.alertViewStyle = 2;
+            gotoAlert.show();
+            let tf = gotoAlert.textFieldAtIndex(0);
+            tf.text = (realPageNo + pageNoOffsets[currentDocmd5]).toString();
+        }
+    }
+
+    //MARK - Addon Class definition
+    var newAddonClass= JSB.defineClass('GoToPage : JSExtension', {
+        //Mark: - Instance Method Definitions
+        // Window initialize
+        sceneWillConnect: function() {
+            if (NSUserDefaults.standardUserDefaults().objectForKey('GoToPage.Offsets')) {
+                pageNoOffsets = NSUserDefaults.standardUserDefaults().objectForKey('GoToPage.Offsets');
+            }
+            // JSB.log('MNLOG pageNoOffsets keys: %@', Object.keys(pageNoOffsets).toString());
+        },
+        // Window disconnect
+        sceneDidDisconnect: function() {
+        },
+        // Window resign active
+        sceneWillResignActive: function() {
+        },
+        // Window become active
+        sceneDidBecomeActive: function() {
+        },
+        //MARK: MN behaviors
+        notebookWillOpen: function(notebookid) {
+        },
+        notebookWillClose: function(notebookid) {
+        },
+        documentDidOpen: function(docmd5) {
+            currentDocmd5 = docmd5
+        },
+        docmentWillClose: function(docmd5) {
+            currentDocmd5 = ''
+        },
+        controllerWillLayoutSubviews: function(controller) {
+        },
+        queryAddonCommandStatus: function() {
+            return {
+                image: 'GoToPage.png',
+                object: self,
+                selector: "toggleGoToPage:",
+                checked: true 
+            };
+        },
+        // Add-On Switch
+        toggleGoToPage: function(sender) {
+            JSB.log('MNLOG toggleGoToPage');
+            let app = Application.sharedInstance();
+            if (app.queryCommandWithKeyFlagsInWindow("p", 0x100000, self.window).disabled) { return }
+
+            var title = "Page Offset nil!"
+            if (pageNoOffsets[currentDocmd5]) {
+                title = 'Page Offset: ' + pageNoOffsets[currentDocmd5];
+            }
+            JSB.log('MNLOG title: %@', title);  
+            let message = 'Ex: Set Offset(2) and Page(10): "2@10"\nSet Page(10): "10"\nSet Offset(2): "2@"'
+            UIAlertView.showWithTitleMessageStyleCancelButtonTitleOtherButtonTitlesTapBlock(title, message, 2, "Go", [], tapBlock);
+        },
+    }, /*Class members*/ {
+        //MARK: - Class Method Definitions
+        addonDidConnect: function() {
+        },
+        addonWillDisconnect: function() {
+        },
+        applicationWillEnterForeground: function() {
+        },
+        applicationDidEnterBackground: function() {
+        },
+        applicationDidReceiveLocalNotification: function(notify) {
+        },
+    });
+    return newAddonClass;
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GoToPage
 mnaddon
+
+Ex: 
+
+- Set Offset(2) and Page(10): "2@10"
+- Set Page(10): "10”
+- Set Offset(2): "2@"
+
